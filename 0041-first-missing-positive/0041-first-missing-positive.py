63939c86942103d75bb6ed25c51c5518da55ca7f
Niteesh Seetaram Naik <niteeshseetaramnaik.23ds@saividya.ac.in>
2026-06-24 13:35:36 +0530

    Time: 16 ms (99.56%), Space: 26.3 MB (8.01%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def firstMissingPositive(self, nums):
         seen = set(nums)
-        for i in range(1, len(nums) + 2):
+        for i in range(1, len(nums)+1):
             if i not in seen:
                 return i
 
