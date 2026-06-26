84c1780638cd607977ed5b9f3727df4e3cdd471c
Niteesh Seetaram Naik <niteeshseetaramnaik.23ds@saividya.ac.in>
2026-06-26 19:26:52 +0530

    Time: 0 ms (100%), Space: 13.2 MB (63.44%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        xor=0
+        for n in nums:
+            xor ^= n
+
+        diff_bit = 1
+
+        while not (xor&diff_bit):
+            diff_bit = diff_bit<<1
+
+        a,b=0,0
+        for n in nums:
+            if diff_bit &n:
+                a=a^n
+            else:
+                b=b^n
+        return [a,b]
+        
