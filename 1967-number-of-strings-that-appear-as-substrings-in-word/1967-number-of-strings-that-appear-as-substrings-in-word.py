bd2e1b9ad9cb003adf0c4b25664ce30fa8a29c75
Niteesh Seetaram Naik <niteeshseetaramnaik.23ds@saividya.ac.in>
2026-06-29 19:16:07 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.82%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numOfStrings(self, patterns, word):
+        c=0
+        for w in patterns:
+            if w in word:
+                c=c+1
+        return c
+
+        
+        
