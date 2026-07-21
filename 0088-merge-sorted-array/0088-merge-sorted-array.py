08690ab95c2b205d98e192e3e5498f1c81728b38
Niteesh Seetaram Naik <niteeshseetaramnaik.23ds@saividya.ac.in>
2026-07-22 00:00:16 +0530

    Time: 1 ms (14.08%), Space: 12.5 MB (0.7%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,7 +2,6 @@ class Solution(object):
     def merge(self, nums1, m, nums2, n):
         nums11 = nums1[:m]
         nums22 = nums2[:n]
-
         nums1[:] = sorted(nums11+nums22)
         
         return nums1
