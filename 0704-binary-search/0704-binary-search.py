90615e254453b67f6ea137a8abbd628e765e4e49
Niteesh Seetaram Naik <niteeshseetaramnaik.23ds@saividya.ac.in>
2026-07-28 18:13:16 +0530

    Time: 3 ms (12.37%), Space: 13.2 MB (96.07%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def search(self, nums, target):
+        
+        for i in range(len(nums)):
+            if target==nums[i]:
+                return i
+        return -1
