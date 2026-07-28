38baaf9f4566378de8f7733577f181477de08e64
Niteesh Seetaram Naik <niteeshseetaramnaik.23ds@saividya.ac.in>
2026-07-28 18:23:22 +0530

    Time: 0 ms (100%), Space: 13.2 MB (96.07%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,7 +1,18 @@
 class Solution(object):
     def search(self, nums, target):
-        
-        for i in range(len(nums)):
-            if target==nums[i]:
-                return i
+        n=len(nums)
+        l=0
+        r=n-1
+
+        while l<=r:
+            mid=(l+r)//2
+
+            if target==nums[mid]:
+                return mid
+
+            elif target >nums[mid]:
+                l=mid+1
+
+            else:
+                r=mid-1
         return -1
