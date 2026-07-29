5153654257da043621d63193c47550877bf644d9
Niteesh Seetaram Naik <niteeshseetaramnaik.23ds@saividya.ac.in>
2026-07-30 00:39:24 +0530

    Time: 0 ms (100%), Space: 13.1 MB (38.35%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,17 +1,41 @@
 class Solution(object):
+    def lowerbound(self,nums,target):
+        n=len(nums)
+        l=0
+        r=n-1
+        ans=n
+
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]>=target:
+                ans=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return ans
+    def upperbound(self,nums,target):
+        n=len(nums)
+        l=0
+        r=n-1
+        ans=n
+
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]>target:
+                ans=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return ans
     def searchRange(self, nums, target):
-        result=[]
-        if target not in nums:
+        lb=self.lowerbound(nums,target)
+        ub=self.upperbound(nums,target)
+
+        if lb==ub:
             return [-1,-1]
+        else:
+            return [lb,ub-1]
         
-        for i in range(len(nums)):
-            if nums[i]==target:
-                result.append(i)
-        if len(result)==1:
-            result.append(result[0])
-        if len(result)>2:
-            return [result[0],result[len(result)-1]]
-        return result
 
 
         
