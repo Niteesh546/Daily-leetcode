8699b55ca44c782941192ff23668a80de3d9a7a7
Niteesh Seetaram Naik <niteeshnaik10@gmail.com>
2026-08-21 00:54:35 +0530

    Time: 0 ms (100%), Space: 12.3 MB (64.16%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,15 @@
 class Solution(object):
+    def findPow(self,x,n):
+        if n==0:
+            return 1
+        a=self.findPow(x,n//2)
+        if n%2==0:
+            return a*a
+        else:
+            return a*a*x
     def myPow(self, x, n):
-        return x**n
+        if n>=0:
+            return self.findPow(x,n)
+        else:
+            return 1/self.findPow(x,n*(-1))
         
