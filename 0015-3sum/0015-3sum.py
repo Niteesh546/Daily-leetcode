a70733b4b8f208eb7ac4fc92feeca9c4244b7e18
Niteesh Seetaram Naik <niteeshnaik10@gmail.com>
2026-09-03 23:17:32 +0530

    Time: 639 ms (55.51%), Space: 22.1 MB (82.3%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,29 @@
 class Solution:
-    def threeSum(self, nums):
-        result=[]
-        nums.sort()
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         n=len(nums)
-
+        nums.sort()
+        print(nums)
+        result=[]
         for i in range(n-2):
-            if nums[i]>0:
-                break
-            if i > 0 and nums[i]==nums[i-1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
+            y=i+1
+            z=n-1
+            while y < z:
+                current_sum = nums[y] + nums[z] + nums[i]
 
-            left,right = i+1,n-1
-            while left < right:
-                total = nums[i]+nums[left]+nums[right]
-
-                if total < 0:
-                    left += 1
-                elif total > 0:
-                    right -=1
+                if current_sum < 0:
+                    y += 1
+                elif current_sum > 0:
+                    z -= 1
                 else:
-                    result.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
+                    result.append([nums[i], nums[y], nums[z]])
+                    while y < z and nums[y] == nums[y + 1]:
+                        y += 1
+                    while y < z and nums[z] == nums[z - 1]:
+                        z -= 1
+                    
+                    y += 1
+                    z -= 1
+                    
         return result
-
-        
